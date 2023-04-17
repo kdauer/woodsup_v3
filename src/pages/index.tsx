a78952277b8d6593aa5d8218719e7b99feff44d7
kdauer <dauer.konstantin@gmail.com>
2023-04-17 14:12:38 +0200

    Disable news again

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,121 +1,128 @@
-import { Flex } from "@chakra-ui/react";
-import { useTranslation } from "next-i18next";
-import { serverSideTranslations } from "next-i18next/serverSideTranslations";
-import nextI18NextConfig from "../../next-i18next.config.js";
-import Head from "next/head";
-import NewsContainer from "../components/NewsContainer";
+import { Flex } from '@chakra-ui/react'
+import Head from 'next/head'
+import { useTranslation } from 'next-i18next'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+
+import nextI18NextConfig from '../../next-i18next.config.js'
+import NewsContainer from '../components/NewsContainer'
 
 const Home = () => {
-  const { t } = useTranslation("common");
+    const { t } = useTranslation('common')
 
-  console.log("Woods Up e.v. - from Potsdam with ❤");
+    console.log('Woods Up e.v. - from Potsdam with ❤')
 
-  return (
-    <>
-      <Head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <link
-          rel="apple-touch-icon"
-          sizes="57x57"
-          href="/apple-icon-57x57.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="60x60"
-          href="/apple-icon-60x60.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="72x72"
-          href="/apple-icon-72x72.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="76x76"
-          href="/apple-icon-76x76.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="114x114"
-          href="/apple-icon-114x114.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="120x120"
-          href="/apple-icon-120x120.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="144x144"
-          href="/apple-icon-144x144.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="152x152"
-          href="/apple-icon-152x152.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-icon-180x180.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="192x192"
-          href="/android-icon-192x192.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="96x96"
-          href="/favicon-96x96.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <meta name="msapplication-TileColor" content="#ffffff" />
-        <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
-        <meta name="theme-color" content="#ffffff" />
-        <title>Woods Up e.V.</title>
-      </Head>
-      <main>
-        <Flex
-          w={"full"}
-          minH="100vh"
-          align="center"
-          justify="center"
-          bgImage={"url(/DSC073482.jpg)"}
-          bgRepeat="no-repeat"
-          bgSize="cover"
-          bgPosition="center"
-        >
-          <NewsContainer />
-        </Flex>
-      </main>
-    </>
-  );
-};
+    return (
+        <>
+            <Head>
+                <meta charSet="utf-8" />
+                <meta
+                    name="viewport"
+                    content="width=device-width,initial-scale=1"
+                />
+                <link
+                    rel="apple-touch-icon"
+                    sizes="57x57"
+                    href="/apple-icon-57x57.png"
+                />
+                <link
+                    rel="apple-touch-icon"
+                    sizes="60x60"
+                    href="/apple-icon-60x60.png"
+                />
+                <link
+                    rel="apple-touch-icon"
+                    sizes="72x72"
+                    href="/apple-icon-72x72.png"
+                />
+                <link
+                    rel="apple-touch-icon"
+                    sizes="76x76"
+                    href="/apple-icon-76x76.png"
+                />
+                <link
+                    rel="apple-touch-icon"
+                    sizes="114x114"
+                    href="/apple-icon-114x114.png"
+                />
+                <link
+                    rel="apple-touch-icon"
+                    sizes="120x120"
+                    href="/apple-icon-120x120.png"
+                />
+                <link
+                    rel="apple-touch-icon"
+                    sizes="144x144"
+                    href="/apple-icon-144x144.png"
+                />
+                <link
+                    rel="apple-touch-icon"
+                    sizes="152x152"
+                    href="/apple-icon-152x152.png"
+                />
+                <link
+                    rel="apple-touch-icon"
+                    sizes="180x180"
+                    href="/apple-icon-180x180.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="192x192"
+                    href="/android-icon-192x192.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="32x32"
+                    href="/favicon-32x32.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="96x96"
+                    href="/favicon-96x96.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="16x16"
+                    href="/favicon-16x16.png"
+                />
+                <meta name="msapplication-TileColor" content="#ffffff" />
+                <meta
+                    name="msapplication-TileImage"
+                    content="/ms-icon-144x144.png"
+                />
+                <meta name="theme-color" content="#ffffff" />
+                <title>Woods Up e.V.</title>
+            </Head>
+            <main>
+                <Flex
+                    w="full"
+                    minH="100vh"
+                    align="center"
+                    justify="center"
+                    bgImage="url(/DSC073482.jpg)"
+                    bgRepeat="no-repeat"
+                    bgSize="cover"
+                    bgPosition="center"
+                >
+                    {/* <NewsContainer /> */}
+                </Flex>
+            </main>
+        </>
+    )
+}
 
 export async function getStaticProps({ locale }) {
-  return {
-    props: {
-      ...(await serverSideTranslations(
-        locale,
-        ["common", "news"],
-        nextI18NextConfig
-      )),
-    },
-  };
+    return {
+        props: {
+            ...(await serverSideTranslations(
+                locale,
+                ['common', 'news'],
+                nextI18NextConfig
+            )),
+        },
+    }
 }
-export default Home;
+export default Home
