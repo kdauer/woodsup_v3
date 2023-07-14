d33a0860537d96bb00acf8fb85efd3c5c22b6dc0
konstantin <konstantin.dauer@ambrosys.de>
2023-07-14 20:16:33 +0200

    ♻️ Update not-found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -8,46 +8,42 @@ import NextLink from 'next/link'
 
 export default function NotFound() {
     return (
-        <html lang="en">
-            <body>
-                <Flex align="center" justify="center" h="100vh" w="full">
-                    <Box textAlign="center" py={10} px={6}>
-                        <Heading
-                            display="inline-block"
-                            as="h2"
-                            size={['lg', 'xl', '2xl', '3xl']}
-                            bg="gradientHeading"
-                            backgroundClip="text"
-                        >
-                            404
-                        </Heading>
-                        <Text mt={3} mb={2}>
-                            Page Not Found
-                        </Text>
-                        <Text
-                            fontSize={['xs', 'sm', 'md', 'lg']}
-                            color="gray.500"
-                            mb={6}
-                        >
-                            The page you're looking for does not seem to exist
-                        </Text>
-                        <NextLink href="/">
-                            <Button
-                                size={['sm', 'md', 'lg', 'lg']}
-                                color="white"
-                                bg="gradientButton"
-                                fontFamily="body"
-                                _hover={{
-                                    background: 'black',
-                                    color: 'brand.500',
-                                }}
-                            >
-                                Take me to the home page
-                            </Button>
-                        </NextLink>
-                    </Box>
-                </Flex>
-            </body>
-        </html>
+        <Flex align="center" justify="center" h="100vh" w="full">
+            <Box textAlign="center" py={10} px={6}>
+                <Heading
+                    display="inline-block"
+                    as="h2"
+                    size={['lg', 'xl', '2xl', '3xl']}
+                    bg="gradientHeading"
+                    backgroundClip="text"
+                >
+                    404
+                </Heading>
+                <Text mt={3} mb={2}>
+                    Page Not Found
+                </Text>
+                <Text
+                    fontSize={['xs', 'sm', 'md', 'lg']}
+                    color="gray.500"
+                    mb={6}
+                >
+                    The page you're looking for does not seem to exist
+                </Text>
+                <NextLink href="/">
+                    <Button
+                        size={['sm', 'md', 'lg', 'lg']}
+                        color="white"
+                        bg="gradientButton"
+                        fontFamily="body"
+                        _hover={{
+                            background: 'black',
+                            color: 'brand.500',
+                        }}
+                    >
+                        Take me to the home page
+                    </Button>
+                </NextLink>
+            </Box>
+        </Flex>
     )
 }
