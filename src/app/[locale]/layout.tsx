8be12e44957cf08702f14c5e1a09f0bf75d79136
kdauer <dauer.konstantin@gmail.com>
2025-09-01 20:52:31 +0200

    ♻️ Fix layout pages

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,11 +1,15 @@
+import { Theme } from '@radix-ui/themes'
 import '@radix-ui/themes/styles.css'
+import { oswald, publicSans } from 'app/fonts'
 import { Footer } from 'components/Footer'
 import { NavBar } from 'components/NavBar'
 import { routing } from 'i18n/routing'
 import { NextIntlClientProvider } from 'next-intl'
 import { getMessages } from 'next-intl/server'
+import { ThemeProvider } from 'next-themes'
 import { notFound } from 'next/navigation'
 import { ReactNode } from 'react'
+import '../globals.css'
 
 export default async function LocaleLayout({
     children,
@@ -15,18 +19,38 @@ export default async function LocaleLayout({
     params: Promise<{ locale: string }>
 }) {
     const { locale } = await params
+
     if (!routing.locales.includes(locale as any)) {
         notFound()
     }
+
     const messages = await getMessages()
 
     return (
-        <NextIntlClientProvider locale={locale} messages={messages}>
-            {/* @ts-expect-error Server Component */}
-            <NavBar locale={locale} />
-            {children}
-            {/* @ts-expect-error Server Component */}
-            <Footer locale={locale} />
-        </NextIntlClientProvider>
+        <html
+            lang={locale}
+            className={`${publicSans.variable} ${oswald.variable}`}
+            suppressHydrationWarning
+        >
+            <body>
+                <ThemeProvider
+                    attribute="class"
+                    defaultTheme="system"
+                    enableSystem
+                    disableTransitionOnChange
+                >
+                    <Theme accentColor="green" grayColor="gray">
+                        <NextIntlClientProvider
+                            locale={locale}
+                            messages={messages}
+                        >
+                            <NavBar />
+                            {children}
+                            <Footer />
+                        </NextIntlClientProvider>
+                    </Theme>
+                </ThemeProvider>
+            </body>
+        </html>
     )
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,5 @@
-import { Theme } from '@radix-ui/themes'
-import '@radix-ui/themes/styles.css'
 import { Analytics } from '@vercel/analytics/react'
-import { oswald, publicSans } from 'app/fonts'
 import { Metadata } from 'next'
-import { ThemeProvider } from 'next-themes'
-import './globals.css'
 
 export const metadata: Metadata = {
     title: 'Woods Up e.V.',
@@ -16,30 +11,14 @@ export const metadata: Metadata = {
 }
 
 export default async function RootLayout({
-    // Layouts must accept a children prop.
-    // This will be populated with nested layouts or pages
     children,
-    params,
 }: {
     children: React.ReactNode
-    params: { locale: string }
 }) {
     return (
-        <html
-            lang={params.locale}
-            className={`${publicSans.variable} ${oswald.variable}`}
-            suppressHydrationWarning
-        >
-            <body>
-                <ThemeProvider attribute="class" defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange>
-                    <Theme accentColor="green" grayColor="gray">
-                        {children}
-                        <Analytics />
-                    </Theme>
-                </ThemeProvider>
-            </body>
-        </html>
+        <>
+            {children}
+            <Analytics />
+        </>
     )
 }
